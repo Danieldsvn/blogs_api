2e972a84dd68633129f94ecb4414a9ad514c5834
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-25 18:15:36 -0300

    Criado migration da tabela Users

---
+++ src/database/models/user.js
@@ -0,0 +1,12 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING, 
+  });
+
+  return User;
+};
+
+module.exports = User;
