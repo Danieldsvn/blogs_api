9a18a24abf4a069879fe9cf6e8c1cad7411f41d3
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-26 13:55:32 -0300

    Remove coluna Id da migration PostCategory

--- src/database/migrations/20220826145551-create-post-category.js
+++ src/database/migrations/20220826145551-create-post-category.js
@@ -1,13 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
+    await queryInterface.createTable('PostCategories', {     
       postId: {
         type: Sequelize.INTEGER,
         references: {
