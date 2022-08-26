7501e021c0dc2f8578bd41165dfd776fb6c6b411
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-26 14:58:10 -0300

    Add: coluna id em User model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,5 +1,6 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
+    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
