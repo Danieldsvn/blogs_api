ccb9c18ed6f14745e5e6b08fcae8c4c00d7c5bc3
Daniel Saraiva <danieldsvn@gmail.com>
2022-09-05 22:02:11 -0300

    lint

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -1,40 +1,44 @@
 const { Category, BlogPost } = require('../database/models');
 
-
-const ValidateCategoryIds = async (ids) => {
-  let isValid = { valid: true };
-   Promise.all(ids.every(async (id) => {
-    await Category.findOne(id) === undefined;
-  })).then((value) => isValid = { valid: !value });
-  return isValid;
-}
+const ValidateCategoryIds = async (ids) => {    
+  const idDoesntExist = ids.every(async (id) => {
+   const result = await Category.findOne({ where: { id } });
+   console.table(`result: ${result}`);
+   return result === undefined;
+  });
+  if (idDoesntExist) return { valid: false };
+  return { valid: true };
+};
 
 const ValidateBody = (request) => {
   const { title, content, categoryIds } = request.body;
   if (!title || !content) {
     return { valid: false, code: 400, message: 'Some required fields are missing' };
   }
-  if(!(ValidateCategoryIds(categoryIds).valid)) {
-    return { valid: false, code: 400, message: '"categoryIds" not found' }
+  if (!(ValidateCategoryIds(categoryIds).valid)) {
+    return { valid: false, code: 400, message: '"categoryIds" not found' };
   }
+  return { valid: true };
 };
 
 const Create = async (request, response) => {
   try {
-  const { title, content, categoryIds } = request.body;
-  if(!(ValidateBody(request).valid)) {
-    const { code, message } = ValidateBody(request);
-    return response.status(code).json({ message });
-  }
-  const newPost = await BlogPost.create({
-    title,
-    content,
-    categoryIds: [...categoryIds],
-  })
-} catch (err) {
-  return response.status(500).json({ message: 'Something gone wrong'});
-}
+    const { title, content, categoryIds } = request.body;
+    if (!(ValidateBody(request).valid)) {
+      const { code, message } = ValidateBody(request);
+      return response.status(code).json({ message });
+    }
 
+    const newPost = await BlogPost.create({
+      title,
+      content,
+      categoryIds: [...categoryIds],
+    });
+
+    return response.status(201).json(newPost);
+  } catch (err) {
+    return response.status(500).json({ message: 'Something gone wrong' });
+  }
 };
 
 module.exports = {
